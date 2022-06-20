c9d575f0038983c821ae5918f7a615f895dcf344
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-06-20 19:41:46 -0300

    Add 'GET /talker' endpoint

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', function (req, res) {
+  const talkerFile = './talker.json';
+  const data = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+  res.status(200).json(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
