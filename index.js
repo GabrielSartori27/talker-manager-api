477c7e46b396f7c06ae3eb0e0752344345566627
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-06-26 20:25:14 -0300

    Add 'PUT /talker/:id' endpoint

--- index.js
+++ index.js
@@ -83,11 +83,16 @@ const validateTalkWatchedAt = (res, talk) => {
 }
 };
 
-const validateTalkRate = (res, talk) => {
+const checkRateValue = (res, talk) => {
   if (!talk.rate && talk.rate !== 0) {
- return res.status(400)
-  .json({ message: 'O campo "rate" é obrigatório' }); 
-}
+    return res.status(400)
+     .json({ message: 'O campo "rate" é obrigatório' }); 
+   }
+};
+
+const validateTalkRate = (res, talk) => {
+  const checkedValue = checkRateValue(res, talk);
+  if (checkedValue) return checkedValue;
   if (talk.rate < 1 || talk.rate > 5 || talk.rate.length > 1) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
